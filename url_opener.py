32ebd254ce1b1b319f30043adf46c3eb22f9126a
Javier Beneito Barquero <malsolo.com@outlook.com>
2021-05-11 12:00:07 +0200

    python-core: separate in functions done.

--- url_opener.py
+++ url_opener.py
@@ -1,8 +1,9 @@
+import sys
 from urllib.request import urlopen
 
 
-def fetch_words():
-    story = urlopen('http://sixty-north.com/c/t.txt')
+def fetch_words(url):
+    story = urlopen(url)
     story_words = []
     for line in story:
         line_words = line.decode('utf-8').split()
@@ -12,15 +13,15 @@ def fetch_words():
     return story_words
 
 
-def print_words(story_words):
-    for word in story_words:
-        print(word)
+def print_items(items):
+    for item in items:
+        print(item)
 
 
-def main():
-    words = fetch_words()
-    print_words(words)
+def main(url):
+    words = fetch_words(url)
+    print_items(words)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
