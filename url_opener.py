fce788dde656543bfef656d9d5c28c0806e9442e
Javier Beneito Barquero <malsolo.com@outlook.com>
2021-05-11 11:26:16 +0200

    url opener strings instead of bytes, plus .gitignore

--- url_opener.py
+++ url_opener.py
@@ -5,7 +5,7 @@ story = urlopen('http://sixty-north.com/c/t.txt')
 story_words = []
 
 for line in story:
-    line_words = line.split()
+    line_words = line.decode('utf-8').split()
     for word in line_words:
         story_words.append(word)
 
