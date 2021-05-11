ab457c8b2b8bf465fa66cd13f1a230a86c042ea7
Javier Beneito Barquero <malsolo.com@outlook.com>
2021-05-11 12:07:01 +0200

    python-core: documentation.

--- url_opener.py
+++ url_opener.py
@@ -1,8 +1,22 @@
+""" Retrieve and print words from a URL.
+
+Usage:
+    python3 url_opener.py <URL>
+"""
 import sys
 from urllib.request import urlopen
 
 
 def fetch_words(url):
+    """ Fetch a list of words fromm a URL.
+
+    Args:
+        url: the URL of a UTF-8 text document.
+
+    Returns:
+        A list of strings containing the words from
+        the document.
+    """
     story = urlopen(url)
     story_words = []
     for line in story:
@@ -14,11 +28,21 @@ def fetch_words(url):
 
 
 def print_items(items):
+    """ Print items one per line.
+
+    Args:
+        items: an iterable series of printable items.
+    """
     for item in items:
         print(item)
 
 
 def main(url):
+    """ Print each word from a text document from a URL.
+
+    Args:
+        url: the URL of a UTF-8 text document.
+    """
     words = fetch_words(url)
     print_items(words)
 
